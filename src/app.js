dc1a9fcb75652fe58568276c7e4cb54d7f875c4e
Camilo García <saldarriagagarciacamilo@gmail.com>
2023-05-02 10:18:25 -0500

    Update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(indexRoutes)
 app.use('/api',employeesRoutes)
